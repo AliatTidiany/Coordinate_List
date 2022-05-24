c5a5d427dd9ba8bca6793ba87012868fae3d8898
Alioune Mbodji <ambodj92@gmail.com>
2022-05-24 08:48:34 +0000

    Mettre la chaine de caractere en liste de mots

---
+++ exo9.py
@@ -0,0 +1,20 @@
+ch="Ce beau pays Un pays que je veux remttre sur pied"
+#remplacer ds cette chaine: question A
+ch1=ch.replace("je veux", "Assim veut")
+##Mettre la chaine de caractere en liste: question B
+liste_mots=ch.split()
+print(liste_mots)
+##deuxieme mehode à faire à la maison
+##C.
+#A partir de liste_mots:
+#- créer un dictionnaire qui contiendra:
+#- comme clés, chaque mot,
+#- comme valeurs, le nombre d'occurence de chaque mot
+#Vous proposerez deux manières de faire.
+dict_mots={}
+for i in liste_mots:
+    if i not in dict_mots.keys():
+        dict_mots[i]=1
+    else:
+        dict_mots[i] +=1
+print(dict_mots)
